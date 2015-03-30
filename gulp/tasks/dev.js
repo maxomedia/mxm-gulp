aad474774f3017e04a7b1c77fce7ba0a2cd597af
tuelsch <tuelsch@gmail.com>
2015-03-30 10:07:05 +0200

    Correct spelling error

--- gulp/tasks/dev.js
+++ gulp/tasks/dev.js
@@ -6,7 +6,7 @@ var tasks = [];
 // Ony add enabled tasks
 if (options.less.build)     tasks.push('less');
 if (options.jade.build)     tasks.push('jade');
-if (options.js.build)       tasks.push('browserfiy');
+if (options.js.build)       tasks.push('browserify');
 if (options.iconfont.build) tasks.push('iconfont');
 
 gulp.task('dev', tasks);

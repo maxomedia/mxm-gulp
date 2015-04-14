26c05c0bd7b9f197d4b7b2d071cb03960085691c
tuelsch <tuelsch@gmail.com>
2015-04-14 14:36:51 +0200

    Only minify bundled scripts

--- gulp/tasks/minify-js.js
+++ gulp/tasks/minify-js.js
@@ -2,10 +2,17 @@ var gulp    = require('gulp');
 var options = require('../options').js;
 var uglify  = require('gulp-uglify');
 var rename  = require('gulp-rename');
+var filename = require('../utils/filename');
 
 gulp.task( 'minify-js', ['browserify'], function() {
-	return gulp.src( options.dest + '/**/*.js' )
-		.pipe( uglify() )
-		//.pipe( rename( { suffix: options.suffix } ) )
-		.pipe( gulp.dest( options.dest ) );
+
+	// Only minify bundled scripts
+	var files = [];
+	for (var i = 0; i < options.bundles.length; i++) {
+		files.push(options.dest + '/' + filename(options.bundles[i].src));
+	}
+
+	return gulp.src(files)
+		.pipe(uglify())
+		.pipe(gulp.dest(options.dest));
 });

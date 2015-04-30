665685e97e40557b4463ec69b5c2cd9c8e69b7d8
tuelsch <tuelsch@gmail.com>
2015-04-30 17:23:45 +0200

    Delete unused test files

--- src/js/m1.js
+++
@@ -1,5 +0,0 @@
-module.exports = function () {
-	window.setTimeout(function () {
-		console.log('module 1');
-	}, 1000);
-};
--- src/js/m3.js
+++
@@ -1,5 +0,0 @@
-module.exports = function () {
-	window.setTimeout(function () {
-		console.log('module 3');
-	}, 1000);
-};

1e15644ae6a2943e06430d70720c3ec4ecf8b0fd
tuelsch <tuelsch@gmail.com>
2015-04-30 17:24:12 +0200

    Adapt test files

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,1 @@
-var modules = [
-	'./m1',
-	'./m2',
-	'./m3',
-	'./m4',
-	'./m5',
-	'./m6'
-];
-
-var async = [];
-
-for(var i = 0; i < modules.length; i++) {
-	if (typeof modules[i] === 'function'){}
-}
-
-console.log(modules);
+console.log('mxm-gulp');

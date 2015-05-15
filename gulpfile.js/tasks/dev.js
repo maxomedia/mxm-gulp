555a50260bb991de3c6ceb34516f1fc9af5c2925
tuelsch <tuelsch@gmail.com>
2015-05-15 15:26:50 +0200

    Concatenate dev and stage to environment, these are really small tasks and can be grouped nicely

--- gulpfile.js/tasks/dev.js
+++
@@ -1,6 +0,0 @@
-var gulp  = require('gulp');
-var events = require('../utils/events');
-
-gulp.task('dev', function() {
-	events.emit('gulp.dev');
-});
--- gulpfile.js/tasks/stage.js
+++
@@ -1,5 +0,0 @@
-var gulp  = require('gulp');
-var options = require('../options');
-var tasks = require('../utils/getTasks').stage();
-
-gulp.task('stage', tasks);

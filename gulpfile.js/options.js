63992286cf672e0835c3e18044d5218323dfbb56
tuelsch <tuelsch@gmail.com>
2015-05-15 15:56:05 +0200

    Make use of context option

--- gulpfile.js/options.js
+++ gulpfile.js/options.js
@@ -63,8 +63,11 @@ var options = {
 		// Use paths starting with './' (this folder)
 		// or '../' (this folders parent)
 		entry: {
-			app: './src/js/app.js'
-		}
+			app: './app.js'
+		},
+
+		// See https://github.com/greypants/gulp-starter/pull/139/files
+		context: require('path').resolve('src/js'),
 	},
 
 	jade: {

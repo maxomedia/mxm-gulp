bc65f6e9345de665c448638910a2e94e67eb534c
tuelsch <tuelsch@gmail.com>
2015-06-05 13:04:41 +0200

    Add bower task

---
+++ gulpfile.js/tasks/bower.js
@@ -0,0 +1,16 @@
+var gulp  = require('gulp');
+var bower = require('gulp-bower');
+
+/**
+ * Install bower components
+ * @return {Stream} Gulp stream
+ */
+var bowerInstall = function () {
+	return bower().pipe(gulp.dest('lib/'));
+};
+
+// Register task
+gulp.task('bower', bowerInstall);
+
+// Export core function
+module.exports = bowerInstall;

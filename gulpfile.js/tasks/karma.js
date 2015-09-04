f31ce180dd71301a3f407808c2d9e09aea89ffc1
tuelsch <tuelsch@gmail.com>
2015-09-04 17:28:47 +0200

    Stop cleaning the assets folder, this leads to many unexpected results in the backend

--- gulpfile.js/tasks/karma.js
+++ gulpfile.js/tasks/karma.js
@@ -16,7 +16,7 @@ var karmaTest = function (callback) {
 }
 
 // Register task
-gulp.task('karma', ['install', 'clean'], karmaTest);
+gulp.task('karma', ['install'], karmaTest);
 
 // Export test function
 module.exports = karmaTest;

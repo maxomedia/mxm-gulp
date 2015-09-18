b3edbb5b65db4a49516b54e95d05d3c8080553d8
unknown <skiener@SR-MAXO.mxm.ads>
2015-09-18 16:56:14 +0200

    Added tinypng

--- gulpfile.js/options.js
+++ gulpfile.js/options.js
@@ -99,6 +99,17 @@ var options = {
 		lessDest: 'src/less/core/'
 	},
 
+	tinypng: {
+		//change api key!
+		apikey: "37Y9xQpd3cb1fGBVOfwRSS-D3Ktmi_K7",
+		// SVG files to watch for changes
+		src: 'src/tinypng/',
+		types : '*.{png,jpg,jpeg}',
+		// Destination folder for the less files
+		// containing the mixin
+		dest: 'src/static/img/'
+	},
+
 	// Static webserver and livereload
 	browserSync: {
 

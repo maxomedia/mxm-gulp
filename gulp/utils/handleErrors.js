245e6bab261c6696c66bed30562a57628d9d79d4
unknown <maxomedia@TESTWALL-MAXO.mxm.ads>
2015-11-05 14:57:41 +0100

    Rework notify function to use node-notifier and no longer gulp-notify

--- gulp/utils/handleErrors.js
+++ gulp/utils/handleErrors.js
@@ -1,24 +1,22 @@
-var notify = require("gulp-notify");
 var notifier = require('node-notifier');
 var gutil = require('gulp-util');
 var path = require('path');
-
-module.exports = function(errorObject, callback) {
-  notify.onError(errorObject.toString().split(': ').join(':\n')).apply(this, arguments);
-  // Keep gulp from hanging on this task
-  if (typeof this.emit === 'function') this.emit('end');
-};
 
 // TODO: use this function instead of the ugly one above
 function logError (err) {
 
 	// Windows Balloon, growl or toast notification popup
 	notifier.notify({
-		title: err.title,
+		title: err.title || 'Error: mxm-gulp',
 		message: err.message,
 		icon: path.join(__dirname, 'gulp.png')
 	});
 
 	// Log to the console
 	gutil.log(err.title + '\n' + gutil.colors.red(err.message));
+
+	// Keep gulp from hanging on this task
+  	if (typeof this.emit === 'function') this.emit('end');
 }
+
+module.exports = logError;

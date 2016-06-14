9756eae41c8589e8d30d914b620080281f1dcc17
Stefan Kiener <stefan.kiener@maxomedia.ch>
2016-06-14 09:46:47 +0200

    test files

--- src/ts/demo.ts
+++ src/ts/demo.ts
@@ -1,14 +1,42 @@
 ﻿/// <reference path="./typings/jquery.d.ts"/>
 
 
+//import StringUtil from "./utils/StringUtil";
+
+//import StringUtil = require("./utils/StringUtil");
+
+
 class DemoClass{
-	winEl: JQuery;
+    winEl: JQuery;
+    doc: Document;
 	constructor(){
 		this.winEl = $(window);
-
+        this.doc = document;
+        
 	}
     boom(){
-        alert("Demo !" + this.winEl.width());
+		var main:JQuery = $(".main");
+
+		main.on("click", this.mainClick);
+
+
+        console.log("Demo !" + this.winEl.width());
+        var myArray: Array<Number> = [];
+        myArray.push(0);
+        myArray.push(10);
+
+       /* var strU = new StringUtil();
+        strU.foo();
+        console.log(myArray.join(", ")); */       
+        //var str = strUtil.padLeft("1", "0", 10);
+        //var fooStr = strUtil.foo();
+        //console.log(fooStr);
+
+        //myArray.pus
+
+    }
+    mainClick(event:JQueryEventObject){
+		console.log(event.target);
     }
 }
 
---
+++ src/ts/utils/StringUtil.ts
@@ -0,0 +1,43 @@
+export default class StringUtil{
+	constructor(){
+	}
+	foo():string{
+		return "bar";
+	}
+	/*static padLeft(str:string, padChar:string, count:Number) {
+		if (padChar.length < 1) {
+			return null;
+		}		
+		var arr: Array<string> = str.split("");
+		var tmp: Array<string> = [];
+		for (var i = arr.length; i < count; i++) {
+			tmp.push(padChar);
+		}
+		return tmp.join("") + str;
+	}
+
+	static getUrlParam(url:string, paramName:string, defaultStr:string = null) {
+		var lookFor:string = paramName + "=";
+		var pUrl:string = url;
+		var pIndex:number = pUrl.indexOf(lookFor);
+		if (pIndex == -1) {
+			return defaultStr;
+		}
+		pUrl = pUrl.substr(pIndex + lookFor.length);
+		var ampIndex = pUrl.indexOf("&");
+		var hashIndex = pUrl.indexOf("#");
+
+		if (ampIndex != -1) {
+			return pUrl.substr(0, ampIndex);
+		}
+		if (hashIndex != -1) {
+			return pUrl.substr(0, hashIndex);
+		}
+
+		return pUrl;
+	}*/
+}
+
+//declare var module: any;
+//(module).exports = StringUtil;
+//export = StringUtil;

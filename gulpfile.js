eba2ea447c2b0ba05ba11128132655dd8bf11271
Philipp Gfeller <tuelsch@gmail.com>
2016-12-14 17:07:50 +0100

    Formatting

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var svgSpriteTask = require('./gulp/tasks/svg-sprite');
 var webpackTask   = require('./gulp/tasks/webpack');
 
 gulp.task('default', ['watch']);
-gulp.task('build', ['static','sass','svg-sprite','webpack','pug']);
+gulp.task('build', ['static', 'sass', 'svg-sprite', 'webpack', 'pug']);
 
 gulp.task('watch', ['build', 'serve'], function () {
 	gulp.watch(options.pug.src, ['pug']);

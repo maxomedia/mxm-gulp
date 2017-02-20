76df744e7631be801ab346c94e5ad36cd47b2748
Philipp Gfeller <tuelsch@gmail.com>
2017-02-20 11:28:39 +0100

    Correct reload with browser sync

--- gulp/tasks/handlebars.js
+++ gulp/tasks/handlebars.js
@@ -1,35 +1,36 @@
-var gulp         = require('gulp');
-var hb           = require('gulp-hb');
-var hbLayouts    = require('handlebars-layouts');
-var browserSync  = require('browser-sync');
-var rename       = require('gulp-rename');
-var gutil        = require('gulp-util');
-var plumber      = require('gulp-plumber');
-var options      = require('../options').handlebars;
+var gulp = require('gulp');
+var hb = require('gulp-hb');
+var hbLayouts = require('handlebars-layouts');
+var browserSync = require('./browser-sync').server;
+var rename = require('gulp-rename');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var options = require('../options').handlebars;
 var handleErrors = require('../utils/errorHandler');
 
-function compileHandlebars () {
-	
-	var hbStream = hb()
-	.partials(options.layout)
-	.partials(options.partials)
-	.helpers(hbLayouts)
-	.data(options.data);	
-	// Define source files
-	return gulp.src( options.views )
-		.pipe(plumber(handleErrors))
+function compileHandlebars() {
 
-		// Compile files
-		.pipe(hbStream)
-		.pipe(rename({
-			extname: '.html',
-		}))
+    var hbStream = hb()
+        .partials(options.layout)
+        .partials(options.partials)
+        .helpers(hbLayouts)
+        .data(options.data);
 
-		// Save to destination
-		.pipe( gulp.dest( options.dest ) )
+    // Define source files
+    return gulp.src(options.views)
+        .pipe(plumber(handleErrors))
 
-		// Reolad page
-		.pipe(browserSync.reload({stream: true}));
+    // Compile files
+    .pipe(hbStream)
+        .pipe(rename({
+            extname: '.html',
+        }))
+
+    // Save to destination
+    .pipe(gulp.dest(options.dest))
+
+    // Reolad page
+    .pipe(browserSync.stream());
 }
 
 module.exports = compileHandlebars;
